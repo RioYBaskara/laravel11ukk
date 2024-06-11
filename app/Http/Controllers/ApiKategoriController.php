a0ad35fbd12c64740c945c21c177932ad539a2a3
RioYBaskara <riostembayo664@gmail.com>
2024-06-11 13:49:33 +0700

    [invent-01] Semua Kategori

--- app/Http/Controllers/ApiKategoriController.php
+++ app/Http/Controllers/ApiKategoriController.php
@@ -9,7 +9,7 @@ use App\Models\Kategori;
 class ApiKategoriController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the resource
      */
     public function index()
     {
